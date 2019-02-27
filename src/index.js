4fdc5872b5d4be94dcd1b0fd622bc56e2fa36ee4
Rahul Roy <techniciablog@gmail.com>
2019-02-27 16:01:06 +0530

    Lifting state up from Square component to Boarc component

--- src/index.js
+++ src/index.js
@@ -2,27 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+// Square component is controlled component. Controlled by Board component.
 class Square extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      value: null,
-    };
-  }
-
   render() {
     return (
-      <button className="square" onClick={ () => this.setState({ value: 'X' }) }>
-        { this.state.value }
+      <button className="square" onClick={ () => this.props.onClick() }>
+        { this.props.value }
       </button>
     );
   }
 }
 
 class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
+
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    squares[i] = 'X';
+    this.setState({ squares: squares })
+  }
+
   // renderSquare method is being used from Board's component return method
   renderSquare(i) {
-    return <Square value={i} />;
+    return <Square
+              value={this.state.squares[i]}
+              onClick={ () => this.handleClick(i) }
+           />;
   }
 
   render() {
